54f2b6da693aa3fe15dd8cf3346f2a38ad90ad43
Bryan <bssandovalrod@gmail.com>
2020-07-08 06:52:37 -0500

    Cap 1-Variables

---
+++ variables.py
@@ -0,0 +1,39 @@
+#MI PRIMER COMENTARIO EN PYTHON
+#print("HOLA MUNDO EN PYTHON")
+
+#Int
+a = 1
+b = 2
+#print(type(a))
+
+#Float
+c = 3.1416
+#print(type(c))
+
+#String
+b = "Soy un String o cadena de texto"
+#print(type(b))
+
+#Char
+char = 8364
+#print(chr(char))
+
+#Constantes
+#En python las constantes no existen, sólo se simulan
+PI = 3.1416
+#print(type(PI))
+#print(a+PI)
+
+#If
+x = 5
+y = 2
+#Si x es menor a 3
+if(x > 3):
+  #Crea la variable suma
+  suma = x + y
+  #Imprime valor de la suma
+  print('La suma es '+str(suma))
+#Si no
+else:
+  #Imprime que x es menor a tres
+  print('x es menor a tres y su valor es: '+str(x))
